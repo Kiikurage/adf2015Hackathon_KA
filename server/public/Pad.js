2c0709daa1f6fdb2567a71977d55103886a492c1
msakey <msakey@users.noreply.github.com>
2015-03-30 10:05:03 +0900

    Create Pad.js

---
+++ server/public/Pad.js
@@ -0,0 +1,36 @@
+function Pad(id, x, y, dx, dy)
+{
+	if (Pad.getById(id)) return Pad.getById(id);
+	Pad.records_[id] = this;
+
+	this.id = id;
+	this.x = x;
+	this.y = y;
+	this.dx = dx;
+	this.dy = dy;
+};
+
+Pad.records_ = {};
+
+Pad.getById = function(padId) {
+	return this.records_[padId] || null;
+};
+
+Pad.removeById = function(padId) {
+	var records = this.records_,
+		removedPad = records[padId];
+
+	records[padId] = null;
+
+	return removedPad;
+};
+
+Pad.setPosition = function(x, y) {
+	this.x = x;
+	this.y = y;
+};
+
+Pad.update = function() {
+	this.x += this.dx;
+	this.y += this.dy;
+};
