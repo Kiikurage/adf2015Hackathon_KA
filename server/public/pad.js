086ca4e0da073af94bf3283c446cd689f2fedc68
msakey <msakey@users.noreply.github.com>
2015-03-30 10:42:07 +0900

    Update pad.js

--- server/public/pad.js
+++ server/public/pad.js
@@ -1,12 +1,12 @@
-function Pad(id, x, y, dx, dy) {
+function Pad(id, x, y, vx, vy) {
 	if (Pad.getById(id)) return Pad.getById(id);
 	Pad.records_[id] = this;
 
 	this.id = id;
 	this.x = x;
 	this.y = y;
-	this.dx = dx;
-	this.dy = dy;
+	this.vx = vx;
+	this.vy = vy;
 };
 
 Pad.records_ = {};
@@ -30,12 +30,12 @@ Pad.prototype.setPosition = function(x, y) {
 	this.y = y;
 };
 
-Pad.prototype.setVelocity = function(dx, dy) {
-	this.dx = dx;
-	this.dy = dy;
+Pad.prototype.setVelocity = function(vx, vy) {
+	this.vx = vx;
+	this.vy = vy;
 };
 /*
 Pad.prototype.update = function() {
-	this.x += this.dx;
-	this.y += this.dy;
+	this.x += this.vx;
+	this.y += this.vy;
 };*/
